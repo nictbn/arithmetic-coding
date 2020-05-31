f5ef9976ad2c091a103877f24a080a8eab298fa9
nicolae_taban <nicolae.taban@gmail.com>
2020-06-01 01:26:10 +0300

    Add model class

---
+++ AritmeticV2/Coder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AritmeticV2
+{
+    class Coder
+    {
+    }
+
+
+}
---
+++ AritmeticV2/Model.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AritmeticV2
+{
+    class Model
+    {
+        int no_of_chars;
+        int eof_symbol;
+        int no_of_symbols;
+        int[] char_to_index;
+        int[] index_to_char;
+        int[] cumulative_frequencies;
+        int[] frequencies;
+        
+        public Model(int numberOfCharacters)
+        {
+            no_of_chars = numberOfCharacters;
+            eof_symbol = no_of_chars + 1;
+            no_of_symbols = no_of_chars + 1;
+            char_to_index = new int[no_of_chars];
+            index_to_char = new int [no_of_symbols + 1];
+            cumulative_frequencies = new int[no_of_symbols + 1];
+            frequencies = new int[no_of_symbols + 1];
+        }
+
+        public void start_model() 
+        {
+            for (int i = 0; i < no_of_chars; i++)
+            {
+                char_to_index[i] = i + 1;
+                index_to_char[i + 1] = i;
+            }
+            for (int i = 0; i < no_of_symbols; i++)
+            {
+                frequencies[i] = 1;
+                cumulative_frequencies[i] = no_of_symbols - i;
+            }
+            frequencies[0] = 0;
+        }
+    }
+}
